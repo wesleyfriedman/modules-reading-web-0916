15e7f433bd17e13605aeca63d04ab4cf29d9b618
wesleyfriedman <friedman.wesley@gmail.com>
2016-10-01 18:07:37 -0400

    Done

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,20 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative 'fancy_dance.rb'
+
+class Dancer
+
+	extend FancyDance::ClassMethods
+	include FancyDance::InstanceMethods
+
+	include Dance
+	extend MetaDancing
+
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative 'fancy_dance.rb'
+
+class Kid
+
+	extend FancyDance::ClassMethods
+ 	include FancyDance::InstanceMethods
+
+	include Dance
+	extend MetaDancing
+
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
+
+end
